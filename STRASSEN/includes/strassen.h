629207bcfa583943ad2bf1577bcec8eaacc14a42
DJIEMBOU TIENTCHEU VICTOR NICO <Viclegranddab@gmail.com>
2022-09-21 23:13:31 +0100

    first commit with general approach for coding

---
+++ STRASSEN/includes/strassen.h
@@ -0,0 +1,16 @@
+/*
+ * strassen.h
+ */
+
+#ifndef __STRASSEN_H__
+#define __STRASSEN_H__
+
+#include "matrix.h"
+
+class strassen: public Matrix
+{
+    public:
+    static Matrix strassen(Matrix, Matrix)
+}
+
+#endif
